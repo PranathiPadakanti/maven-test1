56bcc57b8dd93da76045303b890c5d1ef280d80c
PranathiPadakanti <pranathipadakanti123@gmail.com>
2024-09-14 19:03:32 +0530

    PRANATHI0001-Wrote the code for adding two numbers

--- src/main/java/com/example/javamavenjunithelloworld/Utility.java
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -4,8 +4,8 @@ public class Utility {
     public static void main(String[] args) {
     @SuppressWarnings("resource")
     Scanner sc=new Scanner(System.in);
-    int a=sc.nextInt();
     int b=sc.nextInt();
+    int a=sc.nextInt();
    System.out.println(a+b);
     }
      
