849a5a6668c2708e5cd8cecab40f864e23ba76bf
PranathiPadakanti <pranathipadakanti123@gmail.com>
2024-09-14 19:13:38 +0530

    PRANATHIP0001-Wrote the code for checking even or odd number

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.js
@@ -0,0 +1,9 @@
+function pranathi(){
+    console.log("I am Pranathi");
+}
+function EvenOrOdd(num){
+    if(num%2==0)
+        console.log(`${num} is a Even number`);
+    else
+        console.log(`${num} is a Odd number`);
+}
