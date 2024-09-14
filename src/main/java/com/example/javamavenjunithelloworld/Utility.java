b835273aeb49ca111b58262adb2b16573d24c438
PranathiPadakanti <pranathipadakanti123@gmail.com>
2024-09-14 19:01:36 +0530

    PRANATHIP0001-Created HelloWorld Program

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,19 @@
+package com.example.javamavenjunithelloworld;
+import java.util.*;
+public class Utility {
+    public static void main(String[] args) {
+    @SuppressWarnings("resource")
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+   System.out.println(a+b);
+    }
+     
+    {
+}
+ void pranathi()
+{
+System.out.println("I am Prananthi");
+}
+
+}
